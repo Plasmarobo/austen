5fed0bf32c65af6a18c2b61e3d02940e2937319d
Austen <plasmarobo@gmail.com>
2014-02-27 08:39:23 -0500

    Config changes

---
+++ db/migrate/20140226020248_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :twitter
+      t.string :google
+      t.string :facebook
+
+      t.timestamps
+    end
+  end
+end
